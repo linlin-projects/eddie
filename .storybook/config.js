2b48be0572fc50e91344ed9d21c8d8fce045a99a
Lin Chen <lin.chen@innovation.hrs.com>
2018-05-25 13:25:53 +0200

    add some addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure } from "@storybook/react";
+import { setOptions } from "@storybook/addon-options";
+
+setOptions({
+    name: "Eddie UI",
+    url: "https://github.com/linlin-projects/eddie"
+});
 
 function loadStories() {
     require("../stories");
